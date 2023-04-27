050ea6f97a8844844c7b390f0ce2357dc4c8a09a
admin <tranthuan3898@gmail.com>
2023-04-28 03:17:25 +0700

    edit main

--- app/src/main/java/com/example/instagramclone/MainActivity.kt
+++ app/src/main/java/com/example/instagramclone/MainActivity.kt
@@ -3,27 +3,32 @@ package com.example.instagramclone
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.instagramclone.ui.theme.InstagramCloneTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            InstagramCloneTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+            val navController: NavHostController = rememberNavController()
+            NavHost(
+                modifier = Modifier,
+                navController = navController,
+                startDestination = "login"
+            ) {
+                composable("main") { MainScreen() }
+                composable("login") {
+                    LoginScreen(navController)
                 }
+                composable("signup") { SignUpScreen() }
             }
         }
     }
@@ -41,3 +46,4 @@ fun DefaultPreview() {
         Greeting("Android")
     }
 }
+
--- app/src/main/java/com/example/instagramclone/MainScreen.kt
+++ app/src/main/java/com/example/instagramclone/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.example.instagramclone
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -12,15 +13,18 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.navigation.NavController
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.instagramclone.screens.profile.ProfileScreen
+import com.example.instagramclone.screens.reels.ReelsScreen
+import com.example.instagramclone.screens.search.SearchScreen
 
 data class NavItem(val route: String, val icon: ImageVector)
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MainScreen() {
 
@@ -31,9 +35,9 @@ fun MainScreen() {
 
     val items = arrayListOf(
         NavItem(route = Home.route, icon = Icons.Filled.Home),
-        NavItem(route = Home.route, icon = Icons.Filled.Search),
+        NavItem(route = Search.route, icon = Icons.Filled.Search),
         NavItem(route = Home.route, icon = Icons.Outlined.Add),
-        NavItem(route = Home.route, icon = Icons.Filled.Check),
+        NavItem(route = Reels.route, icon = Icons.Filled.Check),
         NavItem(route = Profile.route, icon = Icons.Filled.Person),
     )
 
@@ -61,6 +65,14 @@ fun MainScreen() {
         NavHost(modifier = Modifier.padding(it), navController = navController, startDestination = "home"){
             composable(Home.route) { HomeScreen() }
             composable(Profile.route) { ProfileScreen() }
+            composable(Reels.route) { ReelsScreen() }
+            composable(Search.route) { SearchScreen() }
         }
     }
+}
+
+@Preview
+@Composable
+fun MainActivitySection() {
+    MainScreen()
 }
